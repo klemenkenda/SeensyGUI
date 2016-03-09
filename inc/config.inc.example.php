ef527c205623ae55a9e212c3e93385250fb18e7c
Klemen Kenda <klemen.kenda@ijs.si>
2016-03-09 13:22:56 +0100

    Corrections.

---
+++ inc/config.inc.example.php
@@ -0,0 +1,28 @@
+<?PHP
+//---------------------------------------------------------------------
+// FILE: config.inc.php
+// AUTHOR: Klemen Kenda
+// DESCRIPTION: Seensy
+// DATE: 29/01/2016
+// HISTORY:
+//---------------------------------------------------------------------
+
+// mysql config -------------------------------------------------------
+$mysql_user = "dbuser";
+$mysql_pass = "dbpass";
+$mysql_host = "localhost";
+$mysql_dbase = "dbname";
+
+// mail config --------------------------------------------------------
+$webmaster_mail = "klemen.kenda@ijs.si";
+
+// filesystem config --------------------------------------------------
+$filesystem_root = "D:\\root_folder_www";
+
+// qminer config -------------------------------------------------------
+$miner["url"] = "http://127.0.0.1";
+$miner["port"] = 9201;
+$miner["stream_timeout"] = 20;
+$miner["socket_timeout"] = 10;
+
+?>
